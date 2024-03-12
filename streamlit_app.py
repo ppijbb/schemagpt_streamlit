537d9daa4ec7d58616023c5aa44d1b950e734a46
Kevin.Jung <kevin.jung@turingbio.com>
2024-03-12 13:49:02 +0900

    base code updated
    1. add .gitignore
    2. requirements update
    3. streamlit app code updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,113 @@
 import streamlit as st
-from langchain.llms import OpenAI
-import chromadb
-chroma_client = chromadb.Client()
+
+from langchain.agents import initialize_agent, AgentType
+from langchain.callbacks import StreamlitCallbackHandler
+from langchain.chat_models import ChatOpenAI
+from langchain.retrievers.web_research import WebResearchRetriever
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.tools import DuckDuckGoSearchRun
+from langchain.utilities import DuckDuckGoSearchAPIWrapper, GoogleSearchAPIWrapper
+
+
+chroma_client = Chroma(embedding_function=OpenAIEmbeddings(),
+                       persist_directory="./chromadb_oai")
 
 
 st.title('🦜🔗 Quickstart App')
+try:
+    openai_api_key = st.secrets["OPENAI_API_KEY"]
+except Exception as e:
+    with st.sidebar:
+        openai_api_key = st.text_input("OpenAI API Key", key="langchain_search_api_key_openai", type="password")
+        "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+        "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/2_Chat_with_search.py)"
+        "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+col1, col2 = st.columns(2)
 
-openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
+with col1:
+    st.title("🔎 LangChain - Chat with search")
+
+    """
+    In this example, we're using `StreamlitCallbackHandler` to display the thoughts and actions of an agent in an interactive Streamlit app.
+    Try more LangChain 🤝 Streamlit Agent examples at [github.com/langchain-ai/streamlit-agent](https://github.com/langchain-ai/streamlit-agent).
+    """
+
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = [
+            {
+                "role": "assistant",
+                "content": "Hi, I'm a chatbot who can search the web. How can I help you?"
+            }]
+
+    for msg in st.session_state.messages:
+        st.chat_message(msg["role"]).write(msg["content"])
+
+    if prompt := st.chat_input(placeholder="Who won the Women's U.S. Open in 2018?", key=col1):
+        st.session_state.messages.append(
+            {
+                "role": "user",
+                "content": prompt
+            })
+        st.chat_message("user").write(prompt)
+
+        if not openai_api_key:
+            st.info("Please add your OpenAI API key to continue.")
+            st.stop()
+
+        llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=openai_api_key, streaming=True)
+        search = DuckDuckGoSearchRun(name="Search")
+        search_agent = initialize_agent([search], llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, handle_parsing_errors=True)
+        with st.chat_message("assistant"):
+            st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+            response = search_agent.run(st.session_state.messages, callbacks=[st_cb])
+            st.session_state.messages.append(
+                {
+                    "role": "assistant",
+                    "content": response
+                })
+            st.write(response)
 
 
-def generate_response(input_text):
-    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-    st.info(llm(input_text))
+with col2:
+    st.title("🔎 LangChain - Chat with search")
 
+    """
+    In this example, we're using `StreamlitCallbackHandler` to display the thoughts and actions of an agent in an interactive Streamlit app.
+    Try more LangChain 🤝 Streamlit Agent examples at [github.com/langchain-ai/streamlit-agent](https://github.com/langchain-ai/streamlit-agent).
+    """
 
-with st.form('my_form'):
-    text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
-    submitted = st.form_submit_button('Submit')
-    if not openai_api_key.startswith('sk-'):
-        st.warning('Please enter your OpenAI API key!', icon='⚠')
-    if submitted and openai_api_key.startswith('sk-'):
-        generate_response(text)
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = [
+            {
+                "role": "assistant",
+                "content": "Hi, I'm a chatbot who can search the web. How can I help you?"
+            }]
+
+    for msg in st.session_state.messages:
+        st.chat_message(msg["role"]).write(msg["content"])
+
+    if prompt := st.chat_input(placeholder="Who won the Women's U.S. Open in 2018?", key=col2):
+        st.session_state.messages.append(
+            {
+                "role": "user",
+                "content": prompt
+            })
+        st.chat_message("user").write(prompt)
+
+        if not openai_api_key:
+            st.info("Please add your OpenAI API key to continue.")
+            st.stop()
+
+        llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=openai_api_key, streaming=True)
+        search = DuckDuckGoSearchRun(name="Search")
+        search_agent = initialize_agent([search], llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, handle_parsing_errors=True)
+        with st.chat_message("assistant"):
+            st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+            response = search_agent.run(st.session_state.messages, callbacks=[st_cb])
+            st.session_state.messages.append(
+                {
+                    "role": "assistant",
+                    "content": response
+                })
+            st.write(response)
