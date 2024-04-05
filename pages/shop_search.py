868961abc7738e3bd027bf5af96a0ed90917f0e9
ppijbb <ppijbb@naver.com>
2024-04-05 17:39:07 +0900

    pydeck option changed

--- pages/shop_search.py
+++ pages/shop_search.py
@@ -48,6 +48,7 @@ if 'map' not in st.session_state:
             "lat": np.random.randn(10) / 10 + st.session_state.lat,
             "lon": np.random.randn(10) / 10 + st.session_state.lon,
             "size": np.random.randn(10),
+            "height": np.random.randn(10),
             "color": np.random.rand(10, 4).tolist(),
         }
     )
@@ -59,7 +60,8 @@ def add_pin_in_map(lat: float, lon: float, size: float, color: float):
         pd.DataFrame({
             "lat": lat, 
             "lon": lon, 
-            "size": size, 
+            "size": size,
+            "height": np.random.randn(1),
             "color": color
         })
     ], ignore_index=True).drop_duplicates(subset=["lat", "lon"], keep='last')
@@ -109,7 +111,7 @@ if __name__ == "__main__":
                     get_position='[lon, lat]',
                     get_fill_color='[200, 30, 0, 160*size]',
                     radius=200,
-                    get_elevation="size",
+                    get_elevation="height",
                     elevation_scale=50,
                     elevation_range=[0, 1000],
                     pickable=True,
@@ -117,13 +119,13 @@ if __name__ == "__main__":
                     auto_highlight=True,
                     coverage=1
                 ),
-                pdk.Layer(
-                    'ScatterplotLayer',
-                    data=st.session_state.map,
-                    get_position='[lon, lat]',
-                    get_color='[200, 30, 0, 160]',
-                    get_radius=200,
-                ),
+                # pdk.Layer(
+                #     'ScatterplotLayer',
+                #     data=st.session_state.map,
+                #     get_position='[lon, lat]',
+                #     get_color='[200, 30, 0, 160]',
+                #     get_radius=200,
+                # ),
             ],
         ))
         x = st.number_input(label='x',
