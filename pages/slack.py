2488dd93e6aac9c40d8dbbbc77930d47f76d983a
ppijbb <ppijbb@naver.com>
2024-09-07 17:40:56 +0900

    chore: Update Slack "Add to Slack" button URL in slack.py
    Update the URL of the Slack "Add to Slack" button in the slack.py file to include the client ID from the st.secrets. This change ensures that the button links to the correct authorization URL and enhances the user experience by providing a convenient way to connect the app with Slack.

--- pages/slack.py
+++ pages/slack.py
@@ -62,7 +62,7 @@ if st.button('Disconnect'):
     st.session_state['sio'].disconnect()
     st.write('Disconnected from server')
 st.markdown(
-    """
-<a href="https://slack.com/oauth/v2/authorize?client_id=7017291632512.7692024241072&scope=chat:write,chat:write.customize&user_scope=chat:write"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
+    f"""
+<a href="https://slack.com/oauth/v2/authorize?client_id={st.secrets["SLACK_CLIENT_ID"]}&scope=chat:write,chat:write.customize&user_scope=chat:write"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
 """, unsafe_allow_html=True
 )
