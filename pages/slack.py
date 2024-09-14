1bef36951459a9708e396b37c4840fcf1b1a38e3
ppijbb <ppijbb@naver.com>
2024-09-14 21:52:24 +0900

    Refactor slack.py to remove commented out code and update OAuth settings

--- pages/slack.py
+++ pages/slack.py
@@ -15,14 +15,14 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 from slack_bolt.adapter.socket_mode.async_handler import AsyncSocketModeHandler
 
 
-# oauth_settings = AsyncOAuthSettings(
-#     client_id=st.secrets["SLACK_CLIENT_ID"],
-#     client_secret=st.secrets["SLACK_CLIENT_SECRET"],
-#     scopes=["channels:read", "groups:read", "chat:write"],
-#     user_scopes=["user:read"],
-#     installation_store=FileInstallationStore(base_dir="./data/installations"),
-#     state_store=FileOAuthStateStore(expiration_seconds=100, base_dir="./data/states")
-# )
+oauth_settings = AsyncOAuthSettings(
+    # client_id=st.secrets["SLACK_CLIENT_ID"],
+    # client_secret=st.secrets["SLACK_CLIENT_SECRET"],
+    # scopes=["channels:read", "groups:read", "chat:write"],
+    user_scopes=["user:read"],
+    installation_store=FileInstallationStore(base_dir="./data/installations"),
+    state_store=FileOAuthStateStore(expiration_seconds=100, base_dir="./data/states")
+)
 
 app = AsyncApp(
     signing_secret=st.secrets["SLACK_SIGNING_SECRET"],
